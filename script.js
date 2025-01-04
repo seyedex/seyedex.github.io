4ea78539a9d30683d66f2b0343b58fbea2239098
Dmytro Vrashchak <dmitriydokerrr@gmail.com>
2025-01-04 17:49:37 +0100

    robux-shop

---
+++ script.js
@@ -0,0 +1,5 @@
+let link = document.querySelector("#link");
+link.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.open("https://www.youtube.com/@playboicarti", "_self");
+});
